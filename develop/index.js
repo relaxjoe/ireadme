f0c059505ec0877f6d8d1a40a6dbbdc9d4154c1e
Joe <joeyndiebel@gmail.com>
2024-02-27 23:43:45 -0600

    skeleton

---
+++ develop/index.js
@@ -0,0 +1,63 @@
+// Include packages needed for this application
+
+
+// Array of questions for user input
+const questions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a short description of your project:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'How is your project installed?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'How is your project used?',
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'How can others contribute to your project?',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'How can your project be tested?',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose a license for your project:',
+      choices: ['MIT', 'Apache 2.0', 'GPLv3', 'BSD 3-Clause', 'None'],
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username:',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter your email address:',
+    },
+  ];
+  
+
+// Function to write README file
+
+  
+
+// Function to initialize app
+
+
+  // Function call to initialize app
+  init();
---
+++ develop/utils/generateMarkdown.js
@@ -0,0 +1,12 @@
+// Function to return a license badge based on which license is passed in
+
+
+
+// Function to return the license link
+
+
+// Function to return the license section of README
+
+
+
+// Function to generate markdown for README
